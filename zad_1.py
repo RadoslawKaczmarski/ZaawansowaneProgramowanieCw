3b8644bcde7dcacc01f6fb077b751dfe918efbcc
RadoslawKaczmarski <radoslaw.kaczmarski@edu.uekat.pl>
2021-11-28 09:01:22 +0100

    Usunięcie.txt

--- zad_1.py
+++
@@ -1,9 +0,0 @@
-'''2. Dla osób posiadających podstawową wiedzę o pythonie:
-a. Utwórz funkcję, która otrzyma w parametrze listę 5 imion, a następnie
-wyświetli każde z nich.
-'''
-
-#2A
-def WyswietlImie(imiona:list):
-    for imie in imiona:
-        print(imie)
