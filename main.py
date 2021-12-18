b3679b4b8206460e02535c20841406804f95fe4b
RemleZ26 <you@example.com>
2021-12-18 16:25:39 +0100

    Usuniecie

--- main.py
+++
@@ -1,7 +0,0 @@
-
-import pytesseract
-from PIL import Image
-pytesseract.pytesseract.tesseract_cmd = r'D:\Program Files\Tesseract-OCR\tesseract'
-
-print(pytesseract.image_to_string(Image.open('Zdjecia/nowoczesna.jpg')))
-
